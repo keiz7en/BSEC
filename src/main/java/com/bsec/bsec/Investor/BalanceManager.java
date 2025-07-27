5574eaa8448ed3f804f73f4ac44dd0dfb7438d3d
keiz7en <103592242+keiz7en@users.noreply.github.com>
2025-07-27 12:12:42 +0600

    refactor: remove redundant comments and streamline database and UI initialization logic

---
+++ src/main/java/com/bsec/bsec/Investor/BalanceManager.java
@@ -0,0 +1,38 @@
+package com.bsec.bsec.Investor;
+
+import java.math.BigDecimal;
+
+public class BalanceManager {
+    private static BalanceManager instance;
+    private BigDecimal currentBalance = new BigDecimal("0.00");
+
+    private BalanceManager() {
+    }
+
+    public static BalanceManager getInstance() {
+        if (instance == null) {
+            instance = new BalanceManager();
+        }
+        return instance;
+    }
+
+    public BigDecimal getCurrentBalance() {
+        return currentBalance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.currentBalance = balance;
+    }
+
+    public void addFunds(BigDecimal amount) {
+        this.currentBalance = this.currentBalance.add(amount);
+    }
+
+    public void deductFunds(BigDecimal amount) {
+        this.currentBalance = this.currentBalance.subtract(amount);
+    }
+
+    public String getFormattedBalance() {
+        return String.format("৳%.2f", currentBalance);
+    }
+}
