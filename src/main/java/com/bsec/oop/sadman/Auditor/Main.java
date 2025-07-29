baeec1aeeecd75abf5117c1219d132dffd970f28
Sadman Sakib Aloy <sadmansakib.uni@gmail.com>
2025-07-29 10:35:33 +0600

    Fixed package name

---
+++ src/main/java/com/bsec/oop/sadman/Auditor/Main.java
@@ -0,0 +1,14 @@
+package com.bsec.oop.sadman.Auditor;
+
+import javafx.event.ActionEvent;
+
+public class Main
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void goToLogin(ActionEvent actionEvent) {
+    }
+}
