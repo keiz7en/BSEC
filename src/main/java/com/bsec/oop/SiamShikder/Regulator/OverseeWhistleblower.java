aa6ee4ed977e2e6dedc6720d23f61d68d00e4a62
md.siamshikder <shikdersiam172@gmail.com>
2025-08-01 17:59:34 +0600

    add ui navigation and layout updates for regulator modules

---
+++ src/main/java/com/bsec/oop/SiamShikder/Regulator/OverseeWhistleblower.java
@@ -0,0 +1,26 @@
+package com.bsec.oop.SiamShikder.Regulator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class OverseeWhistleblower {
+
+    @FXML
+    private void goBack(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/com/bsec/oop/SiamShikder/Regulator/Menu_Regulator.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Regulator Menu");
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
