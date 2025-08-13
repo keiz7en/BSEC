3e13b345f4ad4189148e85042a5163c980b66056
md.siamshikder <shikdersiam172@gmail.com>
2025-08-13 19:46:13 +0600

    review and approve market fee proposal and its impact analysis

--- src/main/java/com/bsec/oop/HelloApplication.java
+++ src/main/java/com/bsec/oop/HelloApplication.java
@@ -11,6 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/bsec/summer25section2/Login.fxml"));
+        fxmlLoader.setClassLoader(HelloApplication.class.getClassLoader());
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("BSEC Login");
         stage.setScene(scene);
