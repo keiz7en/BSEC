e107a4b784ebf37140f83f5a38babba93721d000
Sadman Sakib Aloy <sadmansakib.uni@gmail.com>
2025-08-15 20:29:23 +0600

    Updated & improved Controller classes

--- src/main/java/com/bsec/oop/sadman/Company/CompanyMessageAuditorFxmlController.java
+++ src/main/java/com/bsec/oop/sadman/Company/CompanyMessageAuditorFxmlController.java
@@ -1,7 +1,31 @@
 package com.bsec.oop.sadman.Company;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class CompanyMessageAuditorFxmlController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    private ActionEvent event;
+    public void switchScene(ActionEvent event, String fxmlFile) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    @javafx.fxml.FXML
+    public void goBackButt(ActionEvent actionEvent) throws IOException {
+        switchScene(event, "Login.fxml");
+
+    }
+}
--- src/main/java/com/bsec/oop/sadman/Company/Main.java
+++ src/main/java/com/bsec/oop/sadman/Company/Main.java
@@ -1,14 +1,78 @@
 package com.bsec.oop.sadman.Company;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main
 {
+
+
+
     @javafx.fxml.FXML
     public void initialize() {
     }
 
+
+    private static final String COMPANY_BASE_PATH = "/com/bsec/summer25section2/sadman/company/";
+    
+    public void switchScene(ActionEvent event, String fxmlFile) throws IOException {
+        String path = fxmlFile.startsWith("/") ? fxmlFile : COMPANY_BASE_PATH  + fxmlFile;
+        Parent root = FXMLLoader.load(getClass().getResource(path));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
     @javafx.fxml.FXML
-    public void goToLogin(ActionEvent actionEvent) {
+    public void goToLogin(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "/com/bsec/summer25section2/Login.fxml");
+
+    }
+
+    @FXML
+    public void ViewSubmittedReportsButt(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "company_search_ipo.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void UpdateCompanyProfileButt(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "company_update_profile.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void SendQuerytoAuditorButt(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "company_message_auditor.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void UploadComplianceDocumentsButt(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "company_upload_report.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void ViewAuditStatusButt(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "company_view_ipo_status.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void SubmitfinancialreportButt(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "company_submit_ipo.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void DownloadComplianceGuidelinesButt(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "company_download_certificate.fxml");
+    }
+
+    @javafx.fxml.FXML
+    public void ComplianceNoticesButt(ActionEvent actionEvent) throws IOException {
+        switchScene(actionEvent, "company_download_certificate.fxml");
     }
 }
